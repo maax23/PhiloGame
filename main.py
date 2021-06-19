89c65e769c15de9387025f5204237749c2004c07
Hr-ArshA <a_sh1379@yahoo.com>
2021-06-19 19:29:43 +0430

    make base of game

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+
+import pygame, sys
+from scripts.setting import settings
+
+
+pygame.init()
+
+
+surface = pygame.display.set_mode([settings.width, settings.height])
+
+name = pygame.display.set_caption(settings.name)
+
+icon = pygame.image.load(settings.icon)
+pygame.display.set_icon(icon)
+
+
+run = True
+while run:
+     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+
+pygame.quit()
+# sys.exit
