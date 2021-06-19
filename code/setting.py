b45417c581c999a86462f7512689fd37b3531c52
Hr-ArshA <a_sh1379@yahoo.com>
2021-06-19 19:31:58 +0430

    add a class for game setting

---
+++ code/setting.py
@@ -0,0 +1,47 @@
+# import pygame, sys
+
+
+class settings():
+
+    # Window size
+    width = 800
+    height = 600
+
+    # Game name
+    name = 'PhiloGame'
+
+    # Icon for game
+    icon = 'data/img/Logo/favicon-32x32.png'
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pygame.init()
+
+# win = pygame.display.set_mode([800, 600])
+
+# title = pygame.display.set_caption('project_name')
+
+# pine_0 = pygame.image.load('data/img/Logo/favicon-32x32.png')
+
+# pygame.display.set_icon(pine_0)
+
+# run = True
+# while run:
+#      for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+        
+
+# pygame.quit()
+# sys.exit()
